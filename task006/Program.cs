35bd10bb79b8f98351dc4cfc9259fc3c99da2151
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-16 18:10:26 +0300

    принимает на вход число и проверяет, кротно ли оно одновременно 7 и 23

--- task006/Program.cs
+++ task006/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая принимает на вход число и проверяет, кротно ли оно одновременно 7 и 23.
+// Например : 
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.WriteLine("Введите число для проверки : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit1 = num%7;
+int digit2 = num%23;
+if (digit1 == 0 & digit2 ==0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
