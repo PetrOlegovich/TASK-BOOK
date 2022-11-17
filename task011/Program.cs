5c6c8fff16e80e2a2c7e73bb9702f9991ee29cd1
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-17 13:28:47 +0300

    Решаем задачки с задачника уровень 2

---
+++ task011/Program.cs
@@ -0,0 +1,58 @@
+﻿// S2.1. Составить программу:
+// а) вычисления значения функции y=7x2+3x+6 при любом значении x;
+// б) вычисления значения функции x=12a2+7a+12 при любом значении а.
+
+//а)// Console.WriteLine("Введите число Х : ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = 7*x*2+3*x+6;
+// Console.WriteLine($"y = {y}");
+
+// Console.WriteLine("Введите число a : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int x = 12*a*2+7*a+12;
+// Console.WriteLine($"x = {x}");
+
+//S2.2. Дана сторона квадрата. Найти его периметр.
+// Console.WriteLine("Введите сторону квадрата для поиска перимитра : ");
+// double x = Convert.ToDouble(Console.ReadLine());
+// double P = x*4;
+// Console.WriteLine($"Перимитр квадрата равен {P}");
+
+// S2.3. Дан радиус окружности. Найти ее диаметр. 
+
+// Console.WriteLine("Введите радиус окружности для поиска диаметра :");
+// double r = Convert.ToDouble(Console.ReadLine());
+// double D = 2 * r;
+// Console.WriteLine($"Диаметр равен : {D}");
+
+//S2.5. Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности. —
+// Console.WriteLine("Введите сторону куба для вычеслений : ");
+// double x = Convert.ToDouble(Console.ReadLine());
+// double plosh = x*x , ob = x*x*x;
+// Console.WriteLine($"площадь стороны куба равен :{plosh} \nОбьем куба равна {ob}");
+
+//S2.6. Дан радиус окружности. Найти длину окружности и площадь круга.
+
+// Console.Write("Введите радиус : ");
+// double numr = Convert.ToDouble(Console.ReadLine());
+// double numPi = 3.14;
+// double numC = 2*numPi*numr;
+// double numS = numPi*numr*numr;
+// Console.WriteLine($"Длинна окружности = {Math.Round(numC, 2)}\nПлощадь круга = {Math.Round(numS, 2)}");
+
+
+// S2.7. Даны два целых числа. Найти: а) их среднее арифметическое; б) их среднее геометрическое.
+
+
+// Console.Write("Введите первое число \n: ");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите второе число \n: ");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+// double sredA = (num1 + num2)/2;
+// double sredG = Math.Sqrt(num1*num2);
+
+// Console.WriteLine($"среднее арифметическое = {Math.Round(sredA, 3)} \n среднее геометрическое = {Math.Round(sredG, 3)}");
+
+
+
+
