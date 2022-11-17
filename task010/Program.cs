ad1feff546937dc37a12b8ede7bfc6a6104adada
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-17 12:09:10 +0300

    Решаем задачки с задачника уровень  1

--- task010/Program.cs
+++ task010/Program.cs
@@ -10,3 +10,95 @@
 // Console.WriteLine("Введите число : ");
 // int num = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine($"ВЫ ВВЕЛИ ЧИСЛО :  {num}");
+ 
+// Составить программу вывода на экран числа, вводимого с клавиатуры. 
+// После выводимого числа должно следовать сообщение » — вот какое число Вы  ввели».
+
+// Console.WriteLine("Введите число : ");
+//  int num = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine($"{num} - вот такое число вы вввели");
+
+//Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.
+
+// int n1 = 1;
+// int n2 = 13;
+// int n3 = 49;
+// Console.WriteLine($"{n1} {n2} {n3}");
+
+//S1.6. Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
+
+// int n1 = 7, n2 = 15, n3 = 100;
+// Console.WriteLine($"{n1}  {n2}  {n3}");
+
+// S1.7. Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
+
+// int num1 = new Random().Next(1,20);
+// int num2 = new Random().Next(1,20);
+// int num3 = new Random().Next(1,20);
+// Console.WriteLine($"{num1}  {num2}  {num3}");
+
+// S1.8. Составить программу вывода на экран в одну строку четырех любых чисел  с одним пробелом между ними.
+
+
+// int num = new Random().Next(1,20);
+// int num1 = new Random().Next(1,20);
+// int num2 = new Random().Next(1,20);
+// int num3 = new Random().Next(1,20);
+// Console.WriteLine($"{num} {num1} {num2} {num3}");
+
+// S1.9. Вывести на экран числа 50 и 10 одно под другим.
+ 
+// Console.WriteLine(50);
+// Console.WriteLine(10);
+
+//S1.10. Вывести на экран числа 5, 10 и 21 одно под другим.
+
+// Console.WriteLine(5);
+// Console.WriteLine(10);
+// Console.WriteLine(21);
+
+// S1.11. Составить программу вывода на экран «столбиком» четырех любых чисел.
+
+// Console.WriteLine(new Random().Next(1,1000));
+// Console.WriteLine(new Random().Next(1,1000));
+// Console.WriteLine(new Random().Next(1,1000));
+// Console.WriteLine(new Random().Next(1,1000));
+
+//S1.12. Составить программу вывода на экран следующей информации:
+//5 10
+//7 см
+
+//  Console.WriteLine("5 10 \n7 см");
+
+//
+//S1.13. Составить программу вывода на экран следующей информации:
+//2 кг
+//13 17
+
+//  Console.WriteLine("2 кг \n13 17");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
